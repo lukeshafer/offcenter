265e57ea2c513ada1f122823e390e5c27c1efc26
Luke Shafer <lukealanshafer@gmail.com>
2022-09-27 15:19:40 -0400

    Updated image location

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-
 import react from '@astrojs/react';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		ssr: {
-			noExternal: ['normalize'],
-		},
-	},
-	integrations: [svelte(), react()],
+  vite: {
+    ssr: {
+      noExternal: ['normalize']
+    }
+  },
+  integrations: [svelte(), react(), image()]
 });
