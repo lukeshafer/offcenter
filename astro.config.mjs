05d9864af3eceae0ad2c1dc3576a7a08668b3a6e
Luke Shafer <lukealanshafer@gmail.com>
2022-09-27 18:21:42 -0400

    Updated image slideshow to improve accessibility and load times

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import react from '@astrojs/react';
 
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      noExternal: ['normalize']
-    }
-  },
-  integrations: [svelte(), react(), image()]
+	vite: {
+		ssr: {
+			noExternal: ['normalize'],
+		},
+	},
+	integrations: [
+		svelte(),
+		react(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+	],
 });
--- src/data/sections/OurStory.md
+++ src/data/sections/OurStory.md
@@ -1,8 +1,8 @@
 ---
 title: Our Story
 sort: 1
-thumbnail: uploads/tara-liz-joel.webp
-alt: "From left to right: Tara, Liz, and Joel smiling at the camera"
+thumbnail: /uploads/tara-liz-joel.webp
+alt: 'From left to right: Tara, Liz, and Joel smiling at the camera'
 ---
 
 We opened Off Center in 2021, but our story goes all the way back to 2013, when co-owners Joel and Tara became engaged in this very building, back when it was Benito’s.
